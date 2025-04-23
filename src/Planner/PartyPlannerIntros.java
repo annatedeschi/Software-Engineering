9c5d4ca46d0d45669f0b7539f81d9ed82d6701fd
Razzi-Rachenza <rachenzaprosper@gmail.com>
2025-04-23 13:22:30 -0400

    intro

---
+++ src/Planner/PartyPlannerIntros.java
@@ -0,0 +1,84 @@
+package Planner;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class PartyPlannerIntros {
+	static String nameofficial;
+
+	static PartyPlannerHandle Obj2 = new PartyPlannerHandle();
+	public static HashMap<String, String> userInfo = new HashMap<>();
+	 static ArrayList<String> namesf = new ArrayList<>();
+
+	public static void main(String[] args) throws IOException {
+			File O = new File("Femalenames.txt");
+	        Scanner read = new Scanner(O);
+	        while (read.hasNextLine()) {
+		     String data1 = read.nextLine();
+		     namesf.add(data1);}
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Welcome to Event Planner Pro the game!");
+		System.out.println("Are you a returning user? (yes/no)");
+		// trim whitespaces and make lowercases to account for users input
+		String intro = scan.nextLine().trim().toLowerCase();
+		if (intro.equals("yes")) {
+		PartyPlannerHandle.login();
+		} else if (intro.equals("no")) {
+		PartyPlannerHandle.register(intro);
+		} else {
+		System.out.println("ERROR: Invalid input. Please restart, and try again!");
+		scan.close();
+		return;
+		}
+	}
+
+	// rachenza
+	//intro to level 1
+	public static void Babyshower() {
+		int random = (int)(Math.random() * namesf.size()); // range of random numbers from 0 to the size of my array
+		nameofficial = namesf.get(random);
+		System.out.println("You are tasked with planning events for your local neighborhor!\n");
+		System.out.println("Welcome to Level1 " + PartyPlannerHandle.Name + ". First you will begin by planning a baby shower for " + nameofficial + "\n.");
+		}
+
+
+	//rachenza
+	//Lvl 2 Intro
+	public static void Wedding()  {
+		int random = (int)(Math.random() * namesf.size()); // range of random numbers from 0 to the size of my array
+		nameofficial = namesf.get(random);
+
+
+		System.out.println("\nWelcome to Level 2 " + PartyPlannerHandle.Name + "\nIn this level you will be planning a wedding");
+
+		System.out.println("\n" + nameofficial + " wants a fairytale wedding she's been planning for this day since she was 10." +
+		"\nHere is a dialogue about some of the things she wants you to keep in mind: " +
+		"\nHer favorite colors are beige, blue, and gray. " +
+		"\nShe has a very serious nut allergy." +
+		"\nHer family and her inlaws do not get along very well." +
+		"\nAnd she is expecting a decor of high class and elegance." +
+		"\nHer motto is forget about the groom it's always about the bride");
+
+	}
+
+
+
+	//Intro Level 3
+	public static void Quincenera() {
+		int random = (int)(Math.random() * namesf.size());
+        nameofficial = namesf.get(random);
+		System.out.println("\n"
+				+ "\n"
+				+ "Welccome " +PartyPlannerHandle.Name + " To Level 3 " + nameofficial + " Quincenera she has been looking foward to this day since she was 7. "
+				+ "She wants the party to be Fairytale theme, very flashy with lots of flowers and vines. "
+				+ "And she wants a really big dress something that sticks out. "
+				+ "This is a really sopecial day for " + nameofficial );
+
+
+
+	}
+}
